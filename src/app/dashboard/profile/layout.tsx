77fd02fd4d756d29760664ce749cf87d9c714e5f
Kayode Raimi <kayode.raimi123@gmail.com>
2024-07-09 22:55:56 +0100

    second commit

--- src/app/dashboard/profile/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { SessionProvider } from "next-auth/react";
-
-export default function PostLayout({
-  children, // will be a page or nested layout
-}) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
